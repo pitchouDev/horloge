bd1c807492086f2518d20051ffcc413688b6f516
pitchouDev <ibra1louz@gmail.com>
2024-01-06 20:52:16 +0100

    chrono fonctionel, bouton fixe.

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let chrono = document.querySelector("#chrono p");
+let isPlaying = false;
+let intervalID;
+
+let milliseconde = 0;
+let seconde = 0
+let minutes = 0
+
+function play() {
+    if(isPlaying) {
+        chrono.setAttribute("class", "play");
+        isPlaying = false;
+        clearInterval(intervalID)
+    } else {
+        intervalID = setInterval(ActualiserChrono, 10)
+        chrono.setAttribute("class", "pause");
+        isPlaying = true;
+    }
+}
+
+function ActualiserChrono() {
+    milliseconde++;
+    if (milliseconde > 99) {
+        seconde++
+        milliseconde = 0
+        if(seconde > 59) {
+            minutes++
+            seconde = 0
+        }
+    }
+
+    chrono.innerText = `${(minutes < 10) ? `0${minutes}` : `${minutes}`}:${(seconde < 10) ? `0${seconde}` : `${seconde}`}.${(milliseconde < 10) ? `0${milliseconde}` : `${milliseconde}`}`
+    //oe je sais la ligne au dessus c'est du charabia, non en fait à chaque fois on met minutes ou sec ou millsec et on met un 0 avant si c'est plus petit que 10 
+}
